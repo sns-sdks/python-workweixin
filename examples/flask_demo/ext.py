5dab376fffffa272872842b0abf98c728cb0c5e0
ikaroskun <merle.liukun@gmail.com>
2019-10-14 16:19:55 +0800

    WIP(demo): :pencil2: update demo for new code

--- examples/flask_demo/ext.py
+++ examples/flask_demo/ext.py
@@ -1,4 +1,6 @@
 from pywework.api.work import Api
+from pywework.crypto.validation import MessageValidation
+from pywework.message.xml import xml_message
 
 # WeWork
 CORP_ID = 'xx'
@@ -10,6 +12,11 @@ R_AES_KEY = 'xx'
 
 wework = Api(
     corp_id=CORP_ID, corp_secret=CORP_SECRET,
-    agent_id=AGENT_ID,
-    r_token=R_TOKEN, r_aes_key=R_AES_KEY
+    agent_id=AGENT_ID
 )
+
+wework_msg_validation = MessageValidation(
+    token=R_TOKEN, aes_key=R_AES_KEY, receive_id=CORP_ID
+)
+
+wework_msg_xml_parser = xml_message
